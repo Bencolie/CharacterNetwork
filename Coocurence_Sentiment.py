41e95035e89884b02fe8f1c09c5a693768d68b05
Bencolie Barbara <bencoliebarbara@gmail.com>
2024-06-03 20:33:05 +0800

    Cooccurence + Sentiment matrix Calculation

--- Coocurence_Sentiment.py
+++ Coocurence_Sentiment.py
@@ -1,23 +1,42 @@
 # Use venv1
 # lexicon for crowdsourcing sentiment analysis
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
-from nltk.tokenize import word_tokenize 
+from nltk.tokenize import word_tokenize
+from sklearn.feature_extraction.text import CountVectorizer 
 import pandas as pd
+import numpy as np
 
-def interactionsSent(sentences_list:list,characters:set) -> list:
-    interactions_sent = list()
-    for sent in sentences_list:
-        sent_set = set(word_tokenize(sent))
-        multi_characters = sent_set.intersection(characters)
-        if len(multi_characters) >=2:
-            interactions_sent.append(sent)
-    return interactions_sent
-
-def sentimentScore(interactions_sent:list):
-    analyser =  SentimentIntensityAnalyzer()
-    score_list = [analyser.polarity_scores(sent) for sent in interactions_sent]
+# Use vaderSentiment to calculate the sentiment score (compound)
+def sentimentScore(sentence:str) -> float:
+    score_dict = analyser.polarity_scores(sentence)
     """ Comments for debugging """
-    #for idx,sent in enumerate(interactions_sent):
-        #print(f"{sent} : {score_list[idx]}")
-    return score_list
-   
+    #print(f"{sentence} : {score_dict['compound']}")
+    return score_dict['compound']
+# Use CountVectorizer to calculate the occurence of each character in each sentence
+def occrencePerSent(sentences_list:list,characters:list):
+    name_sent_occurence = list()
+    vect = CountVectorizer(vocabulary=characters,binary=True,lowercase=False)
+    occurence_vect = vect.fit_transform(sentences_list).toarray()
+    for value in occurence_vect:
+        float_value = [float(value[i]) for i in range(len(value))]
+        name_sent_occurence.append(float_value)
+    return np.array(name_sent_occurence)
+# Make a Sentiment Socre Matrix
+def sentimentPerSent(sentences_list:list):
+    sentiment_score_list = [sentimentScore(sent) for sent in sentences_list]
+    print(type(sentiment_score_list[0]))
+    return np.array(sentiment_score_list)
+# Do the calculation
+def sentimentANDoccurenceMatrix(sentences_list:list,characters:list):
+    name_sent_occurence = occrencePerSent(sentences_list,characters)
+    sentiment_score = sentimentPerSent(sentences_list)
+    occurence_matrix = np.dot(name_sent_occurence.T,name_sent_occurence)
+    sentiment_matrix = np.dot(name_sent_occurence.T, (name_sent_occurence.T * sentiment_score).T)
+    # 
+    occurence_matrix = np.tril(occurence_matrix)
+    sentiment_matrix = np.tril(sentiment_matrix)
+    # reduce the diagonal value to 0
+    print(occurence_matrix.shape)
+    return sentiment_matrix, occurence_matrix
+
+analyser =  SentimentIntensityAnalyzer()
--- main.py
+++ main.py
@@ -3,13 +3,17 @@ from FileManagement import *
 from SentTokenizer import *
 from NameEntityRecognition import *
 from Coocurence_Sentiment import * 
+from nltk.tokenize import sent_tokenize
 
 def main():
     """
     PartI : NameEntityRecognition
     """
-    # load the text
-    text = readTextData('AliceInWonderland-ch1')
+    
+    #for i in range(1,13):
+    datafile_name = 'AliceInWonderland-ch' + str(12)
+    # load the text 
+    text = readTextData(datafile_name)
     # Sentence Tokenization
     sentences_list = sentTokenizer(text)
     # Select possible character names
@@ -17,13 +21,26 @@ def main():
     # Use frequency to increase accuracy
     characters= freqFilter(candidate,text)
     print(characters)
+    
+    
     """
     PartII : Cooccurence + SentimentAnalysis
     """
-    text = readTextCoref('AliceInWonderland-ch1-coref')
-    sentences_list = sentTokenizer(text)
-    interactions_sent = interactionsSent(sentences_list,characters)
-    sentimentScore(interactions_sent)
+    #for j in range(1,13):
+    coreffile_name = 'AliceInWonderland-coref-ch'+str(12)
+    text = readTextCoref(coreffile_name)
+    sentences_list = sent_tokenize(text)
+    """
+    for sent in sentences_list:
+        print(sent)
+        print('-------------')
+    """
+    """
+    sentiment_matrix,occurence_matrix = sentimentANDoccurenceMatrix(sentences_list,characters)
+    print(sentiment_matrix)
+    print('----------------')
+    print(occurence_matrix)
+    """
     
 if __name__ == "__main__":
     main()
